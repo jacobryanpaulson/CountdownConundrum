14ff7f3afa417ddf4ff78793b59a96ed9665a495
Jacob Paulson <jacobryanpaulson@gmail.com>
2026-07-23 17:51:48 -0400

    added color changing ghost and teleport

---
+++ Assets/Scripts/PlayerTeleport.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    private static bool isOnCooldown = false;
+    [SerializeField] private float teleportCooldown =.5f;
+    private PlayerController playerController;
+
+    private void Awake()
+    {
+        playerController = GetComponent<PlayerController>();
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+           if (!isOnCooldown)
+            {
+                Teleporter teleporter = currentTeleporter.GetComponent<Teleporter>();
+
+                if(teleporter != null)
+                {
+                  StartCoroutine(TeleportRoutine(teleporter.GetDestination().position));
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+            //Uncomment This if you want more portal style and remove the Input Keydown function in update
+            /*if (!isOnCooldown)
+            {
+                Teleporter teleporter = currentTeleporter.GetComponent<Teleporter>();
+
+                if(teleporter != null)
+                {
+                  StartCoroutine(TeleportRoutine(teleporter.GetDestination().position));
+                }
+            }*/
+        }
+    }
+    private IEnumerator TeleportRoutine(Vector3 destinationPosition){
+        isOnCooldown = true;
+         
+         if (playerController != null)
+        {
+            playerController.TeleportTo(destinationPosition);
+        }
+
+        
+
+        yield return new WaitForSeconds(teleportCooldown);
+
+        isOnCooldown = false;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+          if (collision.CompareTag("Teleporter"))
+        {
+            if(collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+
+               
+
+            }
+        }
+    }
+}
