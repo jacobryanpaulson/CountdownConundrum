d4c8ac9a27f9fec7c05f83698fdd061ff4fbcc5a
Michael Mount (pc) <michaeljoesphmount@gmail.com>
2026-07-26 12:40:17 -0400

    stashing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,147 +1,151 @@
-using System.Buffers.Text;
 using System.Collections;
-using System.Numerics;
-using Microsoft.Unity.VisualStudio.Editor;
-using Unity.VisualScripting;
-
 using UnityEngine;
-using UnityEngine.InputSystem.XR.Haptics;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
-
 
 public enum GameState
 {
     MainMenu,
     Playing,
     Pause,
-    GameOver,
-
+    GameOver
 }
-
 
 public class GameManager : MonoBehaviour
 {
     public GameState CurrentState { get; private set; }
-    
-   public static GameManager Instance {get; private set;}
 
-   [Header("Camera References")]
-   public Camera[] puzzleCam;
-   public Transform[] spawnPoints;
-   public LevelGoal[] levelGoals;
-   public int[] puzzleMoveLimits;
-   public GameObject player;
-   public int currentPuzzleIndex = 0;
-   public GameObject menuCanvas;
+    public static GameManager Instance { get; private set; }
 
-   public float levelTransitionDelay = 2.0f;
-   public UnityEngine.UI.Image fadeImage;
-   public float fadeDuration = 1f;
+    [Header("Puzzle References")]
+    public Camera[] puzzleCam;
+    public Transform[] spawnPoints;
+    public LevelGoal[] levelGoals;
+    public int[] puzzleMoveLimits;
+    public GameObject player;
 
-  
+    [Header("Current Puzzle")]
+    public int currentPuzzleIndex;
 
+    [Header("Menu UI")]
+    public GameObject menuCanvas;
 
-   
-  
+    [Header("Level Transition")]
+    public float levelTransitionDelay = 2f;
+    public Image fadeImage;
+    public float fadeDuration = 1f;
 
-    public void PlayButton()
-    {
-       
-        ChangeState(GameState.Playing);
-        
-    }
-    public void QuitButton()
-    {
-        Application.Quit();
+    [Header("Tutorial UI")]
+    [SerializeField] private LevelTutorialUI tutorialUI;
 
-         #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-        #endif
-    }
+    private bool isChangingPuzzle;
 
-    public void OnPause()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-             if (CurrentState == GameState.Playing)
-            {
-                ChangeState(GameState.Pause);
-            }
-            else if (CurrentState == GameState.Pause)
-            {
-                ChangeState(GameState.Playing);
-            }
-        }
-    }
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-
         }
         else
         {
             Destroy(gameObject);
             return;
-
         }
-       
-        
+
+        if (tutorialUI == null)
+        {
+            tutorialUI =
+                FindFirstObjectByType<LevelTutorialUI>(
+                    FindObjectsInactive.Include
+                );
+        }
+
+        if (tutorialUI == null)
+        {
+            Debug.LogError(
+                "GameManager could not find LevelTutorialUI. " +
+                "Make sure the TutorialCanvas has the component attached."
+            );
+        }
     }
+
     private void Start()
     {
-        
-       CurrentState = GameState.MainMenu;
-       
-        HandleStateChange();
-        
-        ResetToFirstPuzzle();
+        CurrentState = GameState.MainMenu;
 
-        if(player != null && player.TryGetComponent<PlayerController>(out PlayerController pc))
-        {
-              if (spawnPoints != null && spawnPoints.Length > 0 && spawnPoints[0] != null)
-        {
-            player.transform.position = spawnPoints[0].position;
-            
-            if (LoopManager.Instance != null)
-            {
-                LoopManager.Instance.UpdateSpawnPoint(spawnPoints[0]);
-            }
-        }
-         if (levelGoals.Length > 0)
-            {
-                pc.UpdatePuzzleReferences(levelGoals[0]);
-            }
-            if(puzzleMoveLimits != null && puzzleMoveLimits.Length > 0)
-            {
-                pc.ResetMoves(puzzleMoveLimits[0]);
-            }
-        }
-       
+        HandleStateChange();
+        ResetToFirstPuzzle();
     }
+
     private void Update()
     {
-        OnPause();
+        HandlePauseInput();
     }
-    public void ChangeState(GameState newState)
+
+    public void PlayButton()
     {
-        if(CurrentState == newState) return;
-         if (newState == GameState.Pause || newState == GameState.Playing && CurrentState == GameState.Pause)
-    {
-        CurrentState = newState;
-        HandleStateChange();
+        ChangeState(GameState.Playing);
     }
-    else
+
+    public void QuitButton()
     {
-        StartCoroutine(TransitionToState(newState));
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    private void HandlePauseInput()
+    {
+        if (!Input.GetKeyDown(KeyCode.Escape))
+        {
+            return;
+        }
+
+        if (CurrentState == GameState.Playing)
+        {
+            ChangeState(GameState.Pause);
+        }
+        else if (CurrentState == GameState.Pause)
+        {
+            ChangeState(GameState.Playing);
         }
     }
-    private IEnumerator TransitionToState(GameState newState)
+
+    public void ChangeState(GameState newState)
     {
-        yield return new WaitForSecondsRealtime(.5f);
+        if (CurrentState == newState)
+        {
+            return;
+        }
+
+        bool isPauseChange =
+            newState == GameState.Pause ||
+            (
+                newState == GameState.Playing &&
+                CurrentState == GameState.Pause
+            );
+
+        if (isPauseChange)
+        {
+            CurrentState = newState;
+            HandleStateChange();
+        }
+        else
+        {
+            StartCoroutine(
+                TransitionToState(newState)
+            );
+        }
+    }
+
+    private IEnumerator TransitionToState(
+        GameState newState
+    )
+    {
+        yield return new WaitForSecondsRealtime(0.5f);
+
         CurrentState = newState;
         HandleStateChange();
     }
@@ -150,181 +154,330 @@ public class GameManager : MonoBehaviour
     {
         switch (CurrentState)
         {
-             case GameState.MainMenu:
-             Time.timeScale = 0;
-              if(menuCanvas != null)
-        {
-            menuCanvas.SetActive(true);
-        }
+            case GameState.MainMenu:
+                Time.timeScale = 0f;
+
+                if (menuCanvas != null)
+                {
+                    menuCanvas.SetActive(true);
+                }
+
                 break;
-             case GameState.Playing:
-             Time.timeScale = 1;
-              if(menuCanvas != null)
-        { 
-            menuCanvas.SetActive(false);
-        }
+
+            case GameState.Playing:
+                Time.timeScale = 1f;
+
+                if (menuCanvas != null)
+                {
+                    menuCanvas.SetActive(false);
+                }
+
+                if (tutorialUI != null)
+                {
+                    tutorialUI.ShowTutorialForLevel(
+                        currentPuzzleIndex
+                    );
+                }
+
                 break;
-             case GameState.Pause:
-             Time.timeScale = 0;
-              
-              if(menuCanvas != null)
-        {
-            menuCanvas.SetActive(true);
-        }
+
+            case GameState.Pause:
+                Time.timeScale = 0f;
+
+                if (menuCanvas != null)
+                {
+                    menuCanvas.SetActive(true);
+                }
+
                 break;
-             case GameState.GameOver:
-             Time.timeScale = 0;
+
+            case GameState.GameOver:
+                Time.timeScale = 0f;
                 break;
         }
     }
+
     public void ResetToFirstPuzzle()
-{
-   
-   
-    currentPuzzleIndex = 0;
-
-     if (levelGoals != null)
     {
-        for (int i = 0; i < levelGoals.Length; i++)
+        currentPuzzleIndex = 0;
+        isChangingPuzzle = false;
+
+        if (tutorialUI != null)
         {
-            if (levelGoals[i] != null)
+            tutorialUI.ResetTutorialProgress();
+        }
+
+        if (levelGoals != null)
+        {
+            for (int i = 0; i < levelGoals.Length; i++)
             {
-                levelGoals[i].ResetGoal();
+                if (levelGoals[i] != null)
+                {
+                    levelGoals[i].ResetGoal();
+                }
+            }
+        }
+
+        if (
+            player != null &&
+            player.TryGetComponent<PlayerController>(
+                out PlayerController playerController
+            )
+        )
+        {
+            if (
+                spawnPoints != null &&
+                spawnPoints.Length > 0 &&
+                spawnPoints[0] != null
+            )
+            {
+                player.transform.position =
+                    spawnPoints[0].position;
+
+                if (LoopManager.Instance != null)
+                {
+                    LoopManager.Instance.UpdateSpawnPoint(
+                        spawnPoints[0]
+                    );
+                }
+            }
+
+            if (
+                levelGoals != null &&
+                levelGoals.Length > 0
+            )
+            {
+                playerController.UpdatePuzzleReferences(
+                    levelGoals[0]
+                );
+            }
+
+            if (
+                puzzleMoveLimits != null &&
+                puzzleMoveLimits.Length > 0
+            )
+            {
+                playerController.ResetMoves(
+                    puzzleMoveLimits[0]
+                );
+            }
+        }
+
+        if (puzzleCam == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < puzzleCam.Length; i++)
+        {
+            if (puzzleCam[i] != null)
+            {
+                puzzleCam[i].gameObject.SetActive(
+                    i == 0
+                );
             }
         }
     }
 
-    
-    if (player != null && player.TryGetComponent<PlayerController>(out PlayerController pc))
-    {
-        
-        if (spawnPoints != null && spawnPoints.Length > 0 && spawnPoints[0] != null)
-        {
-            player.transform.position = spawnPoints[0].position;
-            
-            if (LoopManager.Instance != null)
-            {
-                LoopManager.Instance.UpdateSpawnPoint(spawnPoints[0]);
-            }
-        }
-
-      
-        if (levelGoals != null && levelGoals.Length > 0)
-        {
-            pc.UpdatePuzzleReferences(levelGoals[0]);
-        }
-        if (puzzleMoveLimits != null && puzzleMoveLimits.Length > 0)
-        {
-            pc.ResetMoves(puzzleMoveLimits[0]);
-        }
-    }
-
-    
-    for (int i = 0; i < puzzleCam.Length; i++)
-    {
-        if (puzzleCam[i] != null)
-        {
-            puzzleCam[i].gameObject.SetActive(i == 0);
-        }
-    }
-}
-    
     public void StartDelayedAdvance()
     {
-        StartCoroutine(AdvanceToNextPuzzle());
+        if (isChangingPuzzle)
+        {
+            return;
+        }
+
+        StartCoroutine(
+            AdvanceToNextPuzzle()
+        );
     }
 
     private IEnumerator AdvanceToNextPuzzle()
     {
-        yield return new WaitForSeconds(.5f);
-           if (fadeImage != null)
-    {
-        float elapsedTime = 0f;
-        while (elapsedTime < fadeDuration)
+        isChangingPuzzle = true;
+
+        yield return new WaitForSeconds(0.5f);
+
+        yield return FadeToBlack();
+
+        int nextIndex =
+            currentPuzzleIndex + 1;
+
+        if (
+            puzzleCam == null ||
+            nextIndex >= puzzleCam.Length
+        )
         {
-            elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
-            fadeImage.color = new Color(0, 0, 0, alpha);
-            yield return null;
-        }
-    }
+            Debug.Log("All puzzles completed!");
 
-    
-        
-
-    
-       
-
-        int nextIndex = currentPuzzleIndex + 1;
-        if(nextIndex >= puzzleCam.Length)
-        {
-            ChangeState(GameState.MainMenu);
             ResetToFirstPuzzle();
+
+            if (fadeImage != null)
+            {
+                fadeImage.color =
+                    new Color(0f, 0f, 0f, 0f);
+            }
+
+            CurrentState = GameState.MainMenu;
+            HandleStateChange();
+
+            isChangingPuzzle = false;
+            yield break;
         }
 
-        if(nextIndex < puzzleCam.Length && nextIndex < spawnPoints.Length)
+        if (
+            spawnPoints == null ||
+            nextIndex >= spawnPoints.Length ||
+            spawnPoints[nextIndex] == null
+        )
         {
-              if (levelGoals != null && levelGoals[currentPuzzleIndex] != null)
+            Debug.LogError(
+                $"Missing spawn point for puzzle index {nextIndex}."
+            );
+
+            isChangingPuzzle = false;
+            yield break;
+        }
+
+        if (
+            levelGoals != null &&
+            currentPuzzleIndex < levelGoals.Length &&
+            levelGoals[currentPuzzleIndex] != null
+        )
         {
             levelGoals[currentPuzzleIndex].ResetGoal();
         }
 
-          Camera currentCam = puzzleCam[currentPuzzleIndex];
-          Camera nextCam = puzzleCam[currentPuzzleIndex + 1];
-          Transform nextSpawn = spawnPoints[currentPuzzleIndex + 1];
+        Camera currentCamera =
+            puzzleCam[currentPuzzleIndex];
 
-            if (LoopManager.Instance != null && nextSpawn != null)
+        Camera nextCamera =
+            puzzleCam[nextIndex];
+
+        Transform nextSpawn =
+            spawnPoints[nextIndex];
+
+        if (LoopManager.Instance != null)
         {
-            LoopManager.Instance.UpdateSpawnPoint(nextSpawn);
+            LoopManager.Instance.UpdateSpawnPoint(
+                nextSpawn
+            );
         }
 
+        if (player != null)
+        {
+            player.transform.position =
+                nextSpawn.position;
 
-          
-
-          if(player != null )
+            if (
+                player.TryGetComponent<PlayerController>(
+                    out PlayerController playerController
+                )
+            )
             {
-                player.transform.position = nextSpawn.position;
-               
-                if(player.TryGetComponent<PlayerController>(out PlayerController pc))
+                if (
+                    levelGoals != null &&
+                    nextIndex < levelGoals.Length
+                )
                 {
-                    pc.UpdatePuzzleReferences(levelGoals[nextIndex]);
-
-                    int nextMoveLimit = puzzleMoveLimits[nextIndex];
-                    pc.ResetMoves(nextMoveLimit);
+                    playerController.UpdatePuzzleReferences(
+                        levelGoals[nextIndex]
+                    );
                 }
-                
+
+                if (
+                    puzzleMoveLimits != null &&
+                    nextIndex < puzzleMoveLimits.Length
+                )
+                {
+                    playerController.ResetMoves(
+                        puzzleMoveLimits[nextIndex]
+                    );
+                }
             }
-          
-          if(currentCam != null && nextCam != null)
-            {
-                nextCam.gameObject.SetActive(true);
-                currentCam.gameObject.SetActive(false);
-            }
-
-
-            
-            currentPuzzleIndex++;
-
-
         }
-        else
+
+        if (nextCamera != null)
         {
-            Debug.Log("All puzzles completed!");
+            nextCamera.gameObject.SetActive(true);
         }
-        yield return new WaitForSeconds(levelTransitionDelay);
-        
-         if (fadeImage != null)
+
+        if (currentCamera != null)
+        {
+            currentCamera.gameObject.SetActive(false);
+        }
+
+        currentPuzzleIndex =
+            nextIndex;
+
+        yield return new WaitForSeconds(
+            levelTransitionDelay
+        );
+
+        yield return FadeFromBlack();
+
+        // This must happen after currentPuzzleIndex changes
+        // and after the new level has faded into view.
+        if (tutorialUI != null)
+        {
+            tutorialUI.ShowTutorialForLevel(
+                currentPuzzleIndex
+            );
+        }
+
+        isChangingPuzzle = false;
+    }
+
+    private IEnumerator FadeToBlack()
     {
+        if (fadeImage == null)
+        {
+            yield break;
+        }
+
         float elapsedTime = 0f;
+
         while (elapsedTime < fadeDuration)
         {
             elapsedTime += Time.deltaTime;
-            float alpha = Mathf.Clamp01(1f - (elapsedTime / fadeDuration));
-            fadeImage.color = new Color(0, 0, 0, alpha);
+
+            float alpha = Mathf.Clamp01(
+                elapsedTime / fadeDuration
+            );
+
+            fadeImage.color =
+                new Color(0f, 0f, 0f, alpha);
+
             yield return null;
         }
+
+        fadeImage.color =
+            new Color(0f, 0f, 0f, 1f);
     }
 
+    private IEnumerator FadeFromBlack()
+    {
+        if (fadeImage == null)
+        {
+            yield break;
+        }
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float alpha = Mathf.Clamp01(
+                1f - elapsedTime / fadeDuration
+            );
+
+            fadeImage.color =
+                new Color(0f, 0f, 0f, alpha);
+
+            yield return null;
+        }
+
+        fadeImage.color =
+            new Color(0f, 0f, 0f, 0f);
     }
-  
 }
